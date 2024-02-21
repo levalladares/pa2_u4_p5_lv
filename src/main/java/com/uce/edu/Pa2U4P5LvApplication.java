7664b7134a88692c677c99a34201c74bef82ee5c
Luis <levalladares@uce.edu.ec>
2024-02-20 20:26:18 -0500

    Taller37

---
+++ src/main/java/com/uce/edu/Pa2U4P5LvApplication.java
@@ -0,0 +1,13 @@
+package com.uce.edu;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pa2U4P5LvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pa2U4P5LvApplication.class, args);
+	}
+
+}
