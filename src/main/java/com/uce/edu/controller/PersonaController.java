372009f18334e91a0559c2447a52aaf49649e95e
Luis <levalladares@uce.edu.ec>
2024-02-27 20:01:27 -0500

    Taller39

--- src/main/java/com/uce/edu/controller/PersonaController.java
+++ src/main/java/com/uce/edu/controller/PersonaController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -23,21 +24,30 @@ public class PersonaController {
 
 	// path
 	// GET
-	@GetMapping("/buscar")
-	public String buscarPorCedula(String cedula) {
-
-		return "";
+	//
+	@GetMapping("/buscarPorCedula/{cedulaPersona}")
+	public String buscarPorCedula(@PathVariable("cedulaPersona") String cedula, Model modelo) {
+		Persona persona = this.iPersonaService.buscarPorCedula(cedula);
+		modelo.addAttribute("persona",persona);
+		return "vistaPersona";
 	}
 
-	@PutMapping("/actualizar")
-	public String actualizar() {
-
-		return "";
+	@PutMapping("/actualizar/{cedulaPersona}")
+	public String actualizar(@PathVariable("cedulaPersona") String cedula, Persona persona) {
+		persona.setCedula(cedula);
+		Persona perAux= this.iPersonaService.buscarPorCedula(cedula);
+		perAux.setApellido(persona.getApellido());
+		perAux.setNombre(persona.getNombre());
+		perAux.setCedula(persona.getCedula());
+		perAux.setGenero(persona.getGenero());
+		this.iPersonaService.actualizar(perAux);
+		return "redirect:/personas/buscarTodos";
 	}
 
-	@DeleteMapping("/borrar")
-	public String borrar() {
-		return "";
+	@DeleteMapping("/borrarPorCedula/{cedula}")
+	public String borrar(@PathVariable("cedula") String cedula) {
+		this.iPersonaService.borrarPorCedula(cedula);
+		return "redirect:/personas/buscarTodos";
 	}
 
 	@PostMapping("/guardar")
@@ -50,6 +60,19 @@ public class PersonaController {
 		List<Persona> lista = this.iPersonaService.consultarTodos();
 		modelo.addAttribute("personas", lista);
 		return"vistaListaPersonas";
+	}
+	
+	@PostMapping("/insertar")
+	public String insertar(Persona persona) {
+		this.iPersonaService.guardar(persona);
+		return "redirect:/personas/buscarTodos";
+
+	}
+	
+	@GetMapping("/nuevaPersona")
+	public String mostrarNuevaPersona(Model modelo,Persona persona) {
+		modelo.addAttribute("persona", persona);
+		return"vistaNuevaPersona";
 	}
 
 }
